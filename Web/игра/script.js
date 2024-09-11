1876e959c3a5c240b76a367b2e16a199464780a2
Pocobeer <vall.com3454.71@gmail.com>
2024-09-11 22:58:43 +0300

    цы

--- Web/игра/script.js
+++ Web/игра/script.js
@@ -1,40 +1,46 @@
-const cactus = document.querySelector('#cactus');
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const scoreElement = document.getElementById("score");
+const bestScoreElement = document.getElementById("bestScore");
+let score = 0;
+let bestScore = localStorage.getItem('bestScore') ? parseInt(localStorage.getItem('bestScore')) : 0;
 
-document.addEventListener('keydown', event => {
-    move();
+// Отображаем лучший результат
+bestScoreElement.innerText = `Лучший результат: ${bestScore}`;
+
+// Увеличиваем счет каждую секунду
+setInterval(function () {
+    
+    scoreElement.innerText = `Счет: ${score}`;
+    score++;
+    // Сохраняем лучший результат
+    if (score > bestScore) {
+        bestScore = score;
+        localStorage.setItem('bestScore', bestScore);
+        bestScoreElement.innerText = `Лучший результат: ${bestScore}`;
+    }
+}, 1000);
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
+        setTimeout(function () {
+            dino.classList.remove("jump");
+        }, 300);
+    }
+}
+
+let isAlive = setInterval(function () {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        alert("Game Over! Ваш счет: " + score);
+        score = 0; // Сбрасываем текущий счет
+        scoreElement.innerText = `Счет: ${score}`;
+    }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+    jump();
 });
-
-function move() {
-    if (!cactus.classList.contains('move')) {
-        cactus.classList.add('move');
-    }
-}
-
-const dino = document.querySelector('#dino');
-
-document.addEventListener('keydown', event => {
-    jump(event);
-});
-
-function jump(event) {
-    if (!(event.code === 'Space')) {
-        return;
-    }
-    if (!dino.classList.contains('jump')) {
-        dino.classList.add('jump');
-        setTimeout(function() {dino.classList.remove('jump')}, 2000);
-    }
-}
-
-function isCrashed() {
-    let dinoPosition = dino.getBoundingClientRect();
-    let cactusPosition = cactus.getBoundingClientRect();
-
-    return dinoPosition.right > cactusPosition.left && dinoPosition.left < cactusPosition.right && dinoPosition.bottom > cactusPosition.top;
-}
-
-setInterval(() => {
-    if (isCrashed()) {
-        alert('Game Over!');
-    }
-})
