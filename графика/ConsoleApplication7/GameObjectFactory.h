81156c07fb51f0c392d29f398dc2d7987bed268e
Pocobeer <vall.com3454.71@gmail.com>
2024-10-07 11:44:54 +0300

    d

--- графика/ConsoleApplication7/GameObjectFactory.h
+++ графика/ConsoleApplication7/GameObjectFactory.h
@@ -5,6 +5,17 @@
 #include "Mesh.h"
 #include "PhongMaterial.h"
 #include "GameObject.h"
+
+// ОПРЕДЕЛЕНИЕ ТИПОВ ИГРОВЫХ ОБЪЕКТОВ
+enum class GameObjectType {
+	LIGHT_OBJECT, // легкий игровой объект
+	HEAVY_OBJECT, // тяжелый игровой объект
+	BORDER_OBJECT, // граничный игровой объект
+	PLAYER, // игровой объект для представления игрока
+	BOMB, // игровой объект для представления бомбы
+	MONSTER // игровой объект для представления монстров
+};
+
 // КЛАСС ДЛЯ СОЗДАНИЯ ИГРОВЫХ ОБЪЕКТОВ
 class GameObjectFactory
 {
