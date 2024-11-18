14af0713342f38bf3bba55a83d86d8969aba5074
Pocobeer <vall.com3454.71@gmail.com>
2024-11-18 21:42:37 +0300

    б

--- Филп/1_lfb.lisp
+++ Филп/1_lfb.lisp
@@ -1,4 +1,4 @@
-'((father(Sergey :age 52))
+'((father(Sergey 50))
 (mother(Helen))
 (son(Alex))
 (son(Vladimir))
@@ -6,19 +6,23 @@
 
 (defun start ()
     (f_head)
+    (terpri)
     (f_tail)
+    (terpri)
     (add_f)
+    (terpri)
     (predicat_atom)
+    (terpri)
     (predicat_list)
 )
 
-(defun f_head ()   (write(car '((father(Sergey))(mother(Helen))(son(Alex))(son(Vladimir))(cat(murz))))))
+(defun f_head ()   (write(car '((father(Sergey 50))(mother(Helen 50))(son(Alex 24))(son(Vladimir 20))(cat(murz 5))))))
 
-(defun f_tail () (write(cdr '((father(Sergey))(mother(Helen))(son(Alex))(son(Vladimir))(cat(murz))))))
+(defun f_tail () (write(cdr '((father(Sergey 50))(mother(Helen 50))(son(Alex 24))(son(Vladimir 20))(cat(murz 5))))))
 
-(defun add_f () (write(cons '(babushka(Zoya)) '((father(Sergey))(mother(Helen))(son(Alex))(son(Vladimir))(cat(murz))))))
+(defun add_f () (write(cons '(babushka(Zoya)) '((father(Sergey 50))(mother(Helen 50))(son(Alex 24))(son(Vladimir 20))(cat(murz 5))))))
 
-(defun predicat_atom () (write(atom '((father(Sergey))(mother(Helen))(son(Alex))(son(Vladimir))(cat(murz))))))
+(defun predicat_atom () (write(atom '((father(Sergey 50))(mother(Helen 50))(son(Alex 24))(son(Vladimir 20))(cat(murz 5))))))
 
-(defun predicat_list () (write(list '((father(Sergey))(mother(Helen))(son(Alex))(son(Vladimir))(cat(murz))))))
+(defun predicat_list () (write(list '((father(Sergey 50))(mother(Helen 50))(son(Alex 24))(son(Vladimir 20))(cat(murz 5))))))
 
