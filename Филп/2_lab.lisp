415343dd8bcc881d550f209ff6fddbab9eac4efd
Pocobeer <vall.com3454.71@gmail.com>
2024-12-02 14:31:31 +0300

    1

--- Филп/2_lab.lisp
+++ Филп/2_lab.lisp
@@ -1,8 +1,12 @@
 ((lambda (r) (* pi r r)) 5)
 
-(defun plosh (r) (* pi r r))
+(defun plosh (r) ((lambda (r) (* pi r r)) r))
 
 (defun popadanie (x y) 
     (cond ((and (>= y 0) (<= y (sin x))) t)
     (t nil))
 )
+
+(write (plosh 5))
+(terpri)
+(write (popadanie 1 0.5))
