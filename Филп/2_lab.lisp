edccf1ea315548a462bc5c17c5a5ea696b1e57ba
Pocobeer <vall.com3454.71@gmail.com>
2024-12-02 22:10:45 +0300

    2

--- Филп/2_lab.lisp
+++ Филп/2_lab.lisp
@@ -7,6 +7,23 @@
     (t nil))
 )
 
+(defun popadanie_if (x y) 
+    (if (and (>= y 0) (<= y (sin x))) 
+    t
+    nil)
+)
+
+(defun popadanie_when (x y) 
+    (unless (or (< y 0) (> y (sin x))) 
+    t)
+)
+
+(defun popadanie_unless (x y) 
+    (if (and (>= y 0) (<= y (sin x))) 
+    t
+    nil)
+)
+
 (write (plosh 5))
 (terpri)
 (write (popadanie 1 0.5))
