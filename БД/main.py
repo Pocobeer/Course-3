4ea0528fb6aef2161ac88dfdf56eeffd129dda77
Pocobeer <vall.com3454.71@gmail.com>
2024-12-03 22:11:34 +0300

    q

--- БД/main.py
+++ БД/main.py
@@ -1,6 +1,7 @@
 import pyodbc
 from tkinter import *
 from tkinter import ttk
+from datetime import datetime 
 
 def connect_to_access_db(db_file: str) -> pyodbc.Connection:
     """Устанавливает соединение с базой данных Access."""
@@ -17,27 +18,49 @@ def fetch_all_users(conn: pyodbc.Connection) -> list:
     cursor.close()
     return rows
 
+def format_date(date_value):
+    """Форматирует дату в строку без времени."""
+    if isinstance(date_value, datetime):
+        return date_value.strftime('%Y-%m-%d')  # Форматируем дату как 'ГГГГ-ММ-ДД'
+    return date_value
+
+def tuple_to_string(x):
+    formatted_values = []
+    for item in x:
+        if isinstance(item, datetime):
+            formatted_values.append(format_date(item))  # Форматируем дату
+        else:
+            formatted_values.append(str(item))
+    return tuple(formatted_values)
+
 users = fetch_all_users(conn)
-for user in users:
-    print(user)
+# for user in users:
+#     print(tuple_to_string(user))
 conn.close()
 
-# root = Tk()
-# root.geometry("300x300")
-# root.title("Доступ к данным через высокоуровневый яп")
+root = Tk()
+root.geometry("800x300")
+root.title("Доступ к данным через высокоуровневый яп")
 
-# columns = ('id', 'name', 'registration date','email', 'phone number')
+columns = ('ID_Арендатора', 'ФИО', 'Дата_Регистрации','Email', 'Номер_Телефона')
 
-# userTable = ttk.Treeview(columns=columns, show='headings')
-# userTable.pack(fill=BOTH, expand=1)
+userTable = ttk.Treeview(columns=columns, show='headings')
+userTable.pack(fill=BOTH, expand=1)
 
-# userTable.heading('id', text='id')
-# userTable.heading('name', text='name')
-# userTable.heading('registration date', text='registration date')
-# userTable.heading('email', text='email')
-# userTable.heading('phone number', text='phone number')
+userTable.heading('ID_Арендатора', text='ID_Арендатора')
+userTable.heading('ФИО', text='ФИО')
+userTable.heading('Дата_Регистрации', text='Дата_Регистрации')
+userTable.heading('Email', text='Email')
+userTable.heading('Номер_Телефона', text='Номер_Телефона')
 
-# for user in users:
-#     userTable.insert("", END, values=user)
+for col in columns:
+    userTable.column(col, anchor='center', width=100)  # Устанавливаем ширину по умолчанию
+    userTable.heading(col, text=col)
 
-# root.mainloop()
+for user in users:
+    userTable.insert("", END, values=tuple_to_string(user))
+
+
+Button(root, text="Закрыть", command=root.quit).pack()
+
+root.mainloop()
