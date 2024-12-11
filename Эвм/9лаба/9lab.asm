7a742d736c711c8206602dd5024ae83c9367ff20
Pocobeer <vall.com3454.71@gmail.com>
2024-12-11 17:33:13 +0300

    1

--- Эвм/9лаба/9lab.asm
+++ Эвм/9лаба/9lab.asm
@@ -1,63 +1,86 @@
-; keyboard_service.asm
-include typefile.inc
+; Программа для проверки нажатия клавиш
 .386
-.model FLAT, STDCALL
+.model flat, stdcall
+option casemap:none
+
+include typefile.inc
 
 .data
-msg db 'Key Pressed: ', 0
-key_code db 0
-buffer db 10 dup(0) ; Буфер для хранения кода клавиши
-sound_file db 'keypress.wav', 0 ; Путь к звуковому файлу
+    msgStart db "Start.", 0
+    msgStartLen equ $ - msgStart
+    msgPressed db "Button pressed: ", 0
+    msgEscPressed db "ESC pressed. Exiting...", 0
+    msgNoKeyPressed db "No key pressed.", 0
+    buffer db 256 dup(0)
 
 .code
-_start:
-    ; Основной цикл
-main_loop:
-    ; Проверяем каждую клавишу от 0 до 255
-    mov ecx, 256
-    xor ebx, ebx ; Индекс клавиши
+start:
+    ; Получаем стандартный вывод
+    push STD_OUTPUT_HANDLE
+    call GetStdHandle
 
-check_key:
-    push ebx
-    call GetAsyncKeyState
-    test eax, 8000h ; Проверяем, нажата ли клавиша
-    jz next_key
+    ; Отправляем сообщение о запуске программы
+    push msgStartLen
+    push offset msgStart
+    push eax
+    call WriteConsoleA
+
+    ; Основной цикл программы
+main_loop:
+    ; Проверяем состояние клавиш от 0 до 255
+    mov ecx, 256      ; Устанавливаем счетчик на 256
+    xor ebx, ebx      ; Счетчик клавиш
+
+check_keys:
+    push ebx          ; Сохраняем индекс клавиши
+    call GetAsyncKeyState ; Получаем состояние клавиши
+    test eax, 8000h   ; Проверяем, нажата ли клавиша
+    jz next_key       ; Если не нажата, переходим к следующей клавише
 
     ; Если клавиша нажата, выводим сообщение
-    mov [key_code], bl
-    call display_key
+    push ebx          ; Копируем индекс клавиши для вывода
+    call KeyToString  ; Преобразуем код клавиши в строку
+    push eax          ; Строка с кодом клавиши в стеке
+    push offset msgPressed
+    call WriteConsoleA
+
+    ; Проверяем, нажата ли клавиша ESC (код 27)
+    cmp ebx, 1Bh      ; Код клавиши ESC
+    je exit_program    ; Если нажата ESC, выходим
 
 next_key:
-    pop ebx
-    inc ebx
-    loop check_key
+    pop ebx           ; Восстанавливаем индекс клавиши
+    inc ebx           ; Переходим к следующей клавише
+    loop check_keys   ; Цикл по всем клавишам
 
-    jmp main_loop ; Повторяем цикл
+    ; Если ни одна клавиша не нажата, выводим сообщение
+    push offset msgNoKeyPressed
+    call WriteConsoleA
 
-; Процедура для отображения нажатой клавиши
-display_key proc
-    ; Формируем строку для вывода
-    mov al, [key_code] ; Используем al для байта
-    add al, '0' ; Преобразуем код клавиши в символ
-    mov [buffer], al ; Сохраняем символ в буфер
+    ; Задержка перед следующей проверкой
+    push 100          ; Задержка 100 миллисекунд
+    call Sleep
 
-    ; Выводим сообщение с кодом клавиши
-    push MB_OK
-    push offset buffer
-    push offset msg
-    call MessageBoxA
-
-    ; Воспроизводим звук нажатия клавиши
-    push 0 ; NULL для PlaySound
-    push offset sound_file ; Путь к звуковому файлу
-    push 1 ; SND_ASYNC для асинхронного воспроизведения
-    call PlaySoundA
-
-    ret
-display_key endp
+    ; Возвращаемся к началу цикла
+    jmp main_loop
 
 exit_program:
-    push 0
-    call ExitProcess
+    push offset msgEscPressed
+    call WriteConsoleA  ; Выводим сообщение о выходе
+    push 0            ; Код выхода
+    call ExitProcess   ; Завершение программы
 
-end _start
+KeyToString:
+    ; Преобразование кода клавиши в строку
+    ; Для простоты просто возвращаем код в строковом формате
+    mov eax, ebx      ; Копируем индекс клавиши в eax
+    call itoa         ; Преобразуем число в строку
+    ret
+
+itoa:
+    ; Преобразование числа в строку
+    ; Реализуйте свою логику преобразования
+    lea eax, buffer   ; Возвращаем адрес буфера
+    ret
+
+end start
--- Эвм/9лаба/9lab.bat
+++ Эвм/9лаба/9lab.bat
@@ -2,5 +2,5 @@ tasm32 /ml 9lab.asm
 pause
 tlink32 /Tpe /ap /c /x 9lab.obj 
 pause
-9lab.exe
+td32.exe 9lab.exe
 pause
